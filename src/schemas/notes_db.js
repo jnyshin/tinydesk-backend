6e220129da1eab6b85511e54e3655a5972ef91b0
Yejin Shin <61779849+jnyshin@users.noreply.github.com>
2021-05-24 15:53:50 +0900

    note content type String -> Object

--- src/schemas/notes_db.js
+++ src/schemas/notes_db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const note = new mongoose.Schema({
   // Note title and content
   title: String,
-  content: String,
+  content: Object,
 });
 
 module.exports = mongoose.model("Note", note);
