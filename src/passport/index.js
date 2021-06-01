ca3aaaeb9533f3ac5c19866532d230c0ab4656ce
Fabio Calero <fabio.calero@stonybrook.edu>
2021-06-01 20:32:59 +0900

    login with google

--- src/passport/index.js
+++ src/passport/index.js
@@ -1,43 +1,148 @@
 const passport = require("passport");
-const User = require("../schemas/user");
 const bcrypt = require("bcryptjs");
 const localStrategy = require("passport-local").Strategy;
-module.exports = function () {
-  passport.serializeUser((user, done) => {
-    done(null, user.id);
-  });
-  passport.deserializeUser(async (id, done) => {
-    try {
-      const user = await User.findOne({ _id: id });
-      return done(null, user);
-    } catch (e) {
-      console.error(e);
-      return done(e);
-    }
-  });
-  passport.use(
-    new localStrategy(
-      {
-        usernameField: "email",
-        passwordField: "password",
-        session: true,
-      },
-      async (email, password, done) => {
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require("../schemas/user");
+const Bookmark = require("../schemas/bookmark_db");
+const Folder = require("../schemas/folder_db");
+const Note = require("../schemas/notes_db");
+const Todolist = require("../schemas/todolist_db");
+const Todo = require("../schemas/todo_db");
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+
+module.exports = function() {
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser(async(id, done) => {
         try {
-          const user = await User.findOne({ email: email });
-          if (!user) {
-            return done(null, false, { message: "No such user exists " });
-          }
-          const result = await bcrypt.compare(password, user.password);
-          if (result) {
+            const user = await User.findOne({ _id: id });
             return done(null, user);
-          }
-          done(null, false, { message: "Wrong password" });
-        } catch (error) {
-          console.error(error);
-          done(error);
+        } catch (e) {
+            console.error(e);
+            return done(e);
         }
-      }
-    )
-  );
+    });
+    passport.use(
+        new localStrategy({
+                usernameField: "email",
+                passwordField: "password",
+                session: true,
+            },
+            async(email, password, done) => {
+                try {
+                    const user = await User.findOne({ email: email });
+                    if (!user) {
+                        return done(null, false, { message: "No such user exists " });
+                    }
+                    const result = await bcrypt.compare(password, user.password);
+                    if (result) {
+                        return done(null, user);
+                    }
+                    done(null, false, { message: "Wrong password" });
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
+            }
+        )
+    );
+
+    passport.use(new GoogleStrategy({
+            clientID: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
+            callbackURL: "http://localhost:4000/google/callback"
+        },
+        async (accessToken, refreshToken, profile, cb) => {
+            User.findOne({ googleId: profile.id }, async (err, doc) => {
+
+                console.log('google profile', profile);
+
+                try {
+                    const existingUser = await User.findOne({
+                        where: {googleId: profile.id, provider: 'google'},
+                    });
+                    if (existingUser) {
+                        return cb(null, exUser);
+                    } else {
+                        var user = new User();
+                        user.googleId = profile.id;
+                        user.email = profile.emails[0].value;
+                        user.notes = new Note({ title: "", content: "" });
+                        user.todolists = [new Todolist({ title: "", bookmarks: [] })];
+                        user.folders = new Folder({ title: "", bookmarks: [] });
+                        user.name = profile.displayName;
+                        user.username = profile.emails[0].value;
+                        user.keepUnicorn = true;
+
+                        user.save();
+
+                        console.log("User successfully saved into the database!")
+
+                        return cb(null, user);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    cb(error);
+                }
+                // if (err) {
+                //   return cb(err, null);
+                // }
+          
+                // if (!doc) {
+                //     const newBookmark = new Bookmark({
+                //         title: "Command T Repository",
+                //         url: "https://github.com/janarosmonaliev/project-416",
+                //         color: "green",
+                //         thumbnail: "https://github.githubassets.com/apple-touch-icon-180x180.png",
+                //     });
+                //     await newBookmark.save();
+                //     //make initial todo
+                //     const newTodo = new Todo({ title: "New Todo", isComplete: false });
+                //     await newTodo.save();
+                //     //make an initial folder
+                //     const newFolder = new Folder({
+                //         title: "New Folder",
+                //         bookmarks: [newBookmark._id],
+                //     });
+                //     await newFolder.save();
+                //     //make an initial todolist
+                //     const newTodolist = new Todolist({
+                //         title: "New Todolist",
+                //         todos: [newTodo._id],
+                //     });
+                //     await newTodolist.save();
+                //     //make an initial note
+                //     const newNote = new Note({
+                //         title: "New Npte",
+                //         content: "Welcome to Command T!",
+                //     });
+                //     await newNote.save();
+                    
+                //     const newUser = new User({
+                //         googleId: profile.id,
+                //         email: profile.emails[0].value,
+                //         location: {id: 1843702, name: 'Icheon-si', state: '', country: 'KR', coord: {lon: 127.442497, lat: 37.279171}},
+                //         notes: [newNote._id],
+                //         todolists: [newTodolist._id],
+                //         folders: [newFolder._id],
+                //         backgroundImg: {
+                //             unsplashID: "pic1",
+                //             url: "https://images.unsplash.com/photo-1481414981591-5732874c7193?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjAyNzR8MHwxfHNlYXJjaHw1fHxvcmFuZ2V8ZW58MHwwfHx8MTYxODU1NjAxNQ&ixlib=rb-1.2.1&q=85",
+                //             author: "someone",
+                //         },
+                //         name: profile.name.givenName,
+                //         username: profile.name.givenName,
+                //         keepUnicorn: true,
+                //     });
+          
+                //   await newUser.save();
+                //   cb(null, newUser);
+                // }
+                // cb(null, doc);
+              })
+          }
+        
+    ));
 };
--- src/schemas/user.js
+++ src/schemas/user.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const user = new mongoose.Schema({
   // Email, Location, Password, name, username
+  googleId: {type: String, required: false},
   email: { type: String, required: true },
   location: Object,
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   notes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Note" }],
   todolists: [{ type: mongoose.Schema.Types.ObjectId, ref: "Todolist" }],
   folders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Folder" }],
@@ -11,6 +12,7 @@ const user = new mongoose.Schema({
   name: { type: String, required: true },
   username: { type: String, required: true },
   keepUnicorn: Boolean,
+  
 });
 
 module.exports = mongoose.model("User", user);
