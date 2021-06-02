0ab4b58769beb8db4aeaa1227ea0472a9e0aea6d
Fabio Calero <fabio.calero@stonybrook.edu>
2021-06-03 01:11:18 +0900

    handing over to yejin

--- src/routes/bookmarks.js
+++ src/routes/bookmarks.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require("../schemas/user");
+const Folder = require("../schemas/folder_db");
 const Bookmark = require("../schemas/bookmark_db");
 
 const router = express.Router();
 // @desc    Add a bookmark
 // @route   POST /bookmarks
-router.post("/bookmarks", (req, res) => {
+router.post("/", (req, res) => {
   // code here
-  const userId = req.user._id; 
-  console.log(req.body.title);
+  const folderId = req.body._id;
   const newBookmark = new Bookmark({ title: req.body.title, url: req.body.url, color: req.body.color, thumbnail: req.body.thumbnail });
   newBookmark.save();
   const newId = newBookmark._id;
-  User.updateOne({ _id: userId }, { $push: { folders: newFolder._id } }, 
+  Folder.updateOne({ _id: folderId }, { $push: { bookmarks: newId } }, 
     async (err, doc) => {
         if (err) throw err;
         if (doc) {
@@ -25,13 +24,78 @@ router.post("/bookmarks", (req, res) => {
 
 // @desc    Remove a bookmark
 // @route   DELETE /bookmarks
-router.delete("/bookmarks", (req, res) => {
-  
+router.delete("/", (req, res) => {
+  const folderId = req.body._id;
+    const bookmarkId = req.body.removeId;
+    console.log("got this bookmark's id: ", bookmarkId);
+    Bookmark.deleteOne({ _id: bookmarkId }, async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        console.log(doc);
+      }
+    });
+    Folder.updateOne(
+      { _id: folderId },
+      { $pull: { bookmarks: bookmarkId } },
+      async (err, doc) => {
+        if (err) throw err;
+        if (doc) {
+          console.log("Bookmark deleted");
+          //no more tmp
+          res.send(doc);
+        }
+      }
+    );
 });
 
 // @desc    Change bookmark's position
-// @route   PUT /bookmarks
-router.put("/bookmarks", (req, res) => {
-  // code here
+// @route   PUT /bookmarks/order
+router.put("/order", (req, res) => {
+  const folderId = req.body._id;
+    const bookmarkId = req.body.removeId;
+    const newIndex = req.body.newIndex;
+    console.log("change to position ", newIndex);
+    //remove the original
+    Folder.updateOne(
+      { _id: folderId },
+      { $pull: { bookmarks: bookmarkId } },
+      async (err, doc) => {
+        if (err) throw err;
+        if (doc) {
+          console.log("bookmark pulled ", bookmarkId);
+        }
+        //put the note into new index position
+        Folder.updateOne(
+          { _id: folderId },
+          { $push: { bookmarks: { $each: [bookmarkId], $position: newIndex } } },
+          async (err, doc) => {
+            if (err) throw err;
+            if (doc) {
+              console.log("Bookmark moved to ", newIndex);
+              res.send();
+            }
+          }
+        );
+      }
+    );
 });
+
+// @desc Edit a bookmark
+// @route PUT /bookmarks
+router.put("/", (req, res) => {
+  const bookmarkId = mongoose.Types.ObjectId(req.body._id);
+  console.log("Which bookmark to update in back: ", bookmarkId);
+  Bookmark.updateOne(
+    { _id: bookmarkId },
+    { $set: { title: req.body.title, url: req.body.url, color: req.body.color, thumbnail: req.body.thumbnail } },
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        console.log("Updated Bookmark");
+        res.send(doc);
+      }
+    }
+  );
+});
+
 module.exports = router;
--- src/routes/todos.js
+++ src/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../schemas/user");
 const Todo = require("../schemas/todo_db");
+const Todolist = require("../schemas/todolist_db");
 const mongoose = require("mongoose");
 
 //Router
@@ -9,26 +10,101 @@ const router = express.Router();
 //Routes = /home/todolists
 
 // @desc    Add todo
-// @route   POST /home/users/<String: username>/todolists/<String: todolistId>/todos
-router.post("/todos", (req, res) => {
-  // code here
+// @route   POST /home/users/todos
+router.post("/", (req, res) => {
+  const todolistId = req.body._id; 
+  const newTodo = new Todo({ title: req.body.title, isComplete: false });
+  newTodo.save();
+  const newId = newTodo._id;
+  Todolist.updateOne(
+    { _id: todolistId },
+    { $push: { todos: newId } },
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        console.log("New Todo's id is", newId);
+        console.log(doc);
+        res.send(newId);
+      }
+    }
+  );
 });
 
 // @desc    Remove a completed todo
 // @route   DELETE /home/users/<String: username>/todolists/<String: todolistId>/todos/<String: todoId>
-router.delete("/todos", (req, res) => {
-  // code here
+router.delete("/", (req, res) => {
+  //changed from const tmp = req.session.user
+  const todolistId = req.body._id;
+  const todoId = req.body.removeId;
+  console.log("got this todo's id: ", todoId);
+  Todo.deleteOne({ _id: todoId }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) {
+      console.log(doc);
+    }
+  });
+  //Changed tmp._id
+  Todolist.updateOne(
+    { _id: todolistId },
+    { $pull: { todo: todoId } },
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        res.send(doc);
+      }
+    }
+  );
 });
 
 // @desc    Change todo name
 // @route   PUT /home/users/<String: username>/todolists/<String: todolistId>/todos/<String: todoId>
-router.put("/todos", (req, res) => {
-  // code here
+router.put("/", (req, res) => {
+  const todoId = mongoose.Types.ObjectId(req.body._id);
+  console.log("Which todo to update in back: ", todoId);
+  Todo.updateOne(
+    { _id: todoId },
+    { $set: { title: req.body.title } },
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        console.log("Updated Todo's title");
+        res.send(doc);
+      }
+    }
+  );
 });
 
 // @desc    Change todo's position
 // @route   PUT /home/users/<String: username>/todolists/<String: todolistId>/todos
-router.put("/todos", (req, res) => {
-  // code here
+router.put("/order", (req, res) => {
+  const todolistId = req.body._id;
+  const todoId = req.body.removeId;
+  const newIndex = req.body.newIndex;
+  console.log("change to position ", newIndex);
+  //remove the original
+  Todolist.updateOne(
+    { _id: todolistId },
+    { $pull: { todo: todoId } },
+    async (err, doc) => {
+      if (err) throw err;
+      if (doc) {
+        //User.save();
+        console.log("todo pulled ", todoId);
+      }
+      //put the todolist into new index position
+      Todolist.updateOne(
+        { _id: todolistId },
+        { $push: { todo: { $each: [todoId], $position: newIndex } } },
+        async (err, doc) => {
+          if (err) throw err;
+          if (doc) {
+            //User.save();
+            console.log("todolist pushed into ", todoListId);
+            res.send();
+          }
+        }
+      );
+    }
+  );
 });
 module.exports = router;
