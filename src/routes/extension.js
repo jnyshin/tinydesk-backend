10e5234188aacdc02af854cdcd50b816a797845d
Hasung Jun <59468036+iianjun@users.noreply.github.com>
2021-06-05 20:03:49 +0900

    Update Favicons

--- src/routes/extension.js
+++ src/routes/extension.js
@@ -5,7 +5,7 @@ const User = require("../schemas/user");
 const Folder = require("../schemas/folder_db");
 const Bookmark = require("../schemas/bookmark_db");
 const cookie = require("cookie");
-
+const getFavicons = require("get-website-favicon");
 //Router
 const router = express.Router();
 
@@ -26,16 +26,16 @@ router.post("/", async (req, res) => {
 
   const cookieValue = cookie.parse(req.headers.cookie)[process.env.COOKIE_NAME];
   console.log(cookieValue);
-  // await getFavicons(req.body.url)
-  //   .then((faviconData) => {
-  //     console.log(faviconData);
-  //     if (faviconData.icons.length !== 0) {
-  //       thumbnail = faviconData.icons[faviconData.icons.length - 1].src;
-  //     }
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //   });
+  await getFavicons(req.body.url)
+    .then((faviconData) => {
+      console.log(faviconData);
+      if (faviconData.icons.length !== 0) {
+        thumbnail = faviconData.icons[faviconData.icons.length - 1].src;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
   try {
     Session.findOne({ "session.cookieVal": cookieValue }, async (err, doc) => {
       if (err) console.error(err);
